a5ce921500d79fee91458b35db02045c09517ca2
ParkJiYeoung8297 <wldud8297@gmail.com>
2025-09-20 06:43:39 +0900

    refractor(Delimiter) : 메타 문자 구분을 위해 Pattern.quote()로 변경

--- src/main/java/calculator/domain/Delimiter.java
+++ src/main/java/calculator/domain/Delimiter.java
@@ -3,9 +3,12 @@ package calculator.domain;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * 구분자(사용자 지정 구분자 포함)를 생성하는 책임을 진다.
+ */
 public class Delimiter {
 
-    public static final String DEFAULT_DELIMITER=",|/n";
+    public static final String DEFAULT_DELIMITER=Pattern.quote(",")+"|"+Pattern.quote(":");
     private final String delimiter;
 
     private Delimiter(String delimiter) {
@@ -14,8 +17,8 @@ public class Delimiter {
     }
 
     public static Delimiter buildDelimiter(String inputString) {
-        if (inputString!=null){
-            return new Delimiter(Delimiter.DEFAULT_DELIMITER+"|"+Pattern.quote(inputString));
+        if (!inputString.isEmpty()) {
+            return new Delimiter(Delimiter.DEFAULT_DELIMITER+inputString);
         }
         return new Delimiter(DEFAULT_DELIMITER);
     }
